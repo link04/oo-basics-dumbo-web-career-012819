ddc3954d5f63acd87c7108085cff90587fac174e
Maximo Bautista <max.bautista97@gmail.com>
2019-01-30 16:27:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,10 +3,10 @@ class Book
     @title = title
   end
   
-  attr_accessor:title
-  attr_accessor:author
-  attr_accessor:page_count
-  attr_accessor:genre
+  attr_accessor:title, :author, :page_count, :genre
+  attr_accessor:
+  attr_accessor:
+  attr_accessor
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
