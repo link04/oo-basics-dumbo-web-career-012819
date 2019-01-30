6adaa55314ff78b190478c65d563564a273888b1
Maximo Bautista <max.bautista97@gmail.com>
2019-01-30 16:27:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,8 @@
 class Book
+  attr_accessor:title, :author, :page_count, :genre
   def initialize(title)
     @title = title
   end
-  
-  attr_accessor:title, :author, :page_count, :genre
-  attr_accessor:
-  attr_accessor:
-  attr_accessor
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
